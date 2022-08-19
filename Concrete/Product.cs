3826e07bcb6b52a5b771ac3b5d5b6003cde62629
Lenovo <Lenovo@DESKTOP-HK6IOLA>
2022-08-19 17:32:39 +0300

    Add project files.

---
+++ Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+  public class Product:IEntity
+    {
+        public int ProductId   { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitInStock { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
